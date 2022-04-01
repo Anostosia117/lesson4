f83ccbe4fcdb1dd489742092c4323fc42c27c0ad
Anostosia117 <jigsa@yandex.ru>
2022-04-01 15:48:30 +0300

    Задание 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,14 +11,27 @@ public class Main {
         if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else if
-            (clientOS == 0 && clientDeviceYear < 2015) {
+        (clientOS == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
         if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
         } else if
-        (clientOS == 1 && clientDeviceYear < 2015){
+        (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
+
+
+        // Задание 3
+
+        int year = 2020;
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+
+
     }
 }
